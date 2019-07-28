0e79b0c62daf688841c1b8ec6f7666d61510dac1
ambujkumar22 <51402707+ambujkumar22@users.noreply.github.com>
2019-07-28 16:41:09 +0530

    ProjectEuler-7

--- ProjectEuler-7.py
+++ ProjectEuler-7.py
@@ -1,12 +1,12 @@
 """By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
 What is the 10001st prime number?"""
-
-t = []
-for i in range(2,1000000):
-    for j in range(2,i):
-        if i%j == 0:
-            break
-    else:
-        t.append(i)
-print(t[9999])
+def prime(n):
+    prime = [2]
+    attempt = 3
+    while len(prime) < n:
+        if all(attempt % i != 0 for i in prime):
+            prime.append(attempt)
+        attempt += 2
+    return prime[-1]
+print(prime(6))
--- practice.py
+++ practice.py
@@ -1,6 +1,12 @@
 """By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
 What is the 10001st prime number?"""
-
-a = [x for x in range(2,1000000) if all(x%y != 0 for y in range(2,x))]
-print(a[10000])
+def prime(n):
+    prime = [2]
+    attempt = 3
+    while len(prime) < n:
+        if all(attempt % i != 0 for i in prime):
+            prime.append(attempt)
+        attempt += 2
+    return prime[-1]
+print(prime(6))
