97540dd3620721a638149e87112392728ee60d23
ambujkumar22 <51402707+ambujkumar22@users.noreply.github.com>
2019-08-03 04:44:37 +0530

    ProjecEuler-11

--- practice.py
+++ practice.py
@@ -1,27 +1,8 @@
-"""The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+m = [[8, 2, 22, 97],
+    [3, 5, 78, 9],
+    [6, 1, 36, 14],
+    [9, 12, 32, 11],
+]
 
-Find the sum of all the primes below two million."""
-
-def eratosthenes2(n):
-    #Declare a set - an unordered collection of unique elements
-    multiples = set()
-
-    #Iterate through [2,2000000]
-    for i in range(2, n+1):
-
-        #If i has not been eliminated already 
-        if i not in multiples:
-
-            #Yay prime!
-            yield i
-
-            #Add multiples of the prime in the range to the 'invalid' set
-            multiples.update(range(i*i, n+1, i))
-
-#Now sum it up
-iter = 0
-ml = list(eratosthenes2(2000000))
-for x in ml:
-    iter = int(x) + iter
-
-print(iter)
+product = m[0][3]*m[1][2]*m[2][1]*m[3][0]
+print(product)
