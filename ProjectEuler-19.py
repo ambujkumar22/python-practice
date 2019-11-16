0ea04485504bbc8ad4ecaf30c7a96bde6f330a0c
ambujkumar22 <51402707+ambujkumar22@users.noreply.github.com>
2019-11-17 01:04:34 +0530

    'mendatory'

--- ProjectEuler-19.py
+++ ProjectEuler-19.py
@@ -11,3 +11,16 @@ And on leap years, twenty-nine.
 A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
 
 How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?"""
+
+index = [31,28,31,30,31,30,31,31,30,31,30,31]
+t = []
+days = 0
+for i in range(1900,2001):
+    for j in range(0,12):
+        days += index[j]
+        for k in range(7,index[j], 7):
+            t.append(k)
+print(t)
+
+
+
